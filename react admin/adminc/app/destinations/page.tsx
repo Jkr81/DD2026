07c5649866813ae14ec2cefbd3ebce83f7c79e54
Jkr81 <jkr81@miami.edu>
2026-04-07 22:53:47 -0400

    Permission fix

---
+++ react admin/adminc/app/destinations/page.tsx
@@ -0,0 +1,8 @@
+export default function Destinations() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Destinations Page</h1>
+      <p>This page will list all the destinations.</p>
+    </div>
+  );
+}   
